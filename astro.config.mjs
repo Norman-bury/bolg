8db30816075f651faf81c49b24afac5fc1bb3f33
旬常 <73638307+Norman-bury@users.noreply.github.com>
2024-04-24 18:11:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://ruiboom.cn/",
   base: "/",
   integrations: [
     tailwind(),
